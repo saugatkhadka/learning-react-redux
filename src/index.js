91d67e56904474c3f2d8f207c460a3fc69f8ee4b
saugatkhadka <ksaugat@gmail.com>
2019-10-22 23:21:26 +0545

    Added redux support to react form and list
    Form was connected to the dispatcher and List was connected to the
    mapStateToProps. Can now enter data to the form and update react
    list accordingly

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+// import logger from 'react-logger';
+// add thunk too 
 import store from './js/store/index';
 import App from './js/components/App';
 
 render(
   <Provider store={store}>
-      <App />
+    <App />
   </Provider>,
   document.getElementById("root")
 );
